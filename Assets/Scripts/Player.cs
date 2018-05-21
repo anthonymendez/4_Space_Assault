98b9fae1aa6f4070835e1726572fd861dd97bdc2
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-20 21:40:34 -0400

    S4:L86: Limited player movement to be within the screen. Can be adjusted in the inspector.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour {
     [Tooltip("In meters per second")] [SerializeField] float xSpeed = 4f;
     [Tooltip("In meters per second")] [SerializeField] float ySpeed = 4f;
 
+    [Tooltip("In meters")][Range(0f,100f)][SerializeField] float xRange = 33f;
+    [Tooltip("In meters")] [Range(0f,100f)][SerializeField] float yRange = 20f;
+
     // Use this for initialization
     void Start () {
 		
@@ -15,17 +18,20 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
-
         float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xRawNewPos = transform.localPosition.x + xOffset;
+        float xFixNewPos = Mathf.Clamp(xRawNewPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float yRawNewPos = transform.localPosition.y + yOffset;
+        float yFixNewPos = Mathf.Clamp(yRawNewPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(xFixNewPos, yFixNewPos, transform.localPosition.z);
 
         bool fire1Down = CrossPlatformInputManager.GetButton("Fire1");
         bool fire2Down = CrossPlatformInputManager.GetButton("Fire2");
         bool fire3Down = CrossPlatformInputManager.GetButton("Fire3");
-
-        gameObject.transform.Translate(Vector3.left * xThrow);
-        gameObject.transform.Translate(Vector3.up * yThrow);
 
 
     }
