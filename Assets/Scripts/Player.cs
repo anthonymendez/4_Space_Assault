c9f1622fd108fa3b25cd4130a34fc51072c7ba26
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-20 21:21:37 -0400

    S4:L85: Calculated offset for horizontal and vertical movement. Removed player controls.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    [Tooltip("In meters per second")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In meters per second")] [SerializeField] float ySpeed = 4f;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
         bool fire1Down = CrossPlatformInputManager.GetButton("Fire1");
         bool fire2Down = CrossPlatformInputManager.GetButton("Fire2");
         bool fire3Down = CrossPlatformInputManager.GetButton("Fire3");
 
-        gameObject.transform.Translate(Vector3.left * horizontalThrow);
-        gameObject.transform.Translate(Vector3.up * verticalThrow);
+        gameObject.transform.Translate(Vector3.left * xThrow);
+        gameObject.transform.Translate(Vector3.up * yThrow);
+
+
     }
 }
