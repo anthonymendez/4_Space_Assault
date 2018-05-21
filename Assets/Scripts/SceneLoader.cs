03669b9f4b5ccc69d72524522c00bd499e05a5c0
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-21 15:20:31 -0400

    S4:L95: Reorganized Assets

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+    
+    [Range(0f, 10f)] [SerializeField] float loadTime = 2.5f;
+
+    // Use this for initialization
+    void Start() {
+        Invoke("LoadMainGame", loadTime);
+    }
+
+    private void LoadMainGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
