43ade7c8b39140d4103c96ad1a0bbf4003f725c1
Anthony Mendez <anthonymendez9@gmail.com>
2018-05-21 17:22:30 -0400

    S4:L101: Enemies instantiate their own mesh collider on game start.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    private MeshCollider meshCollider;
+
+    void Start() {
+        AddNonTriggerMeshCollider();
+    }
+
+    private void AddNonTriggerMeshCollider() {
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.isTrigger = false;
+    }
+
     void OnParticleCollision(GameObject other) {
         Destroy(gameObject);
     }
